ca4a36d31af80d41c79595a551bcc1367d400c96
Zoltan <Zoltan@LAPTOP-44QC2000>
2021-06-26 16:26:21 +0200

    MySol

--- src/main/java/it/polito/tdp/imdb/db/DBConnect.java
+++ src/main/java/it/polito/tdp/imdb/db/DBConnect.java
@@ -17,7 +17,7 @@ public class DBConnect {
 			HikariConfig config = new HikariConfig();
 			config.setJdbcUrl(jdbcURL);
 			config.setUsername("root");
-			config.setPassword("Adm1nAcc0unt99");
+			config.setPassword("rootroot");
 			
 			// configurazione MySQL
 			config.addDataSourceProperty("cachePrepStmts", "true");
